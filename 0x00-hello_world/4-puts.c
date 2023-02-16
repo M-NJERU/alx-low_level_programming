636404e0428632c3d42298ac7e4115651d17ac12
wanja <wanjamaria78@gmail.com>
2023-02-16 12:56:07 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#incude <stdio.h>
+/**
+ * main -A C program that prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+{
+puts ("\"Programming is like building a multilingual puzzle")
+return (0)
+}
