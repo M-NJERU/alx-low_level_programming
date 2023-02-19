b5bad04bc6421ff5171bbebe71a9c2cec8274f62
wanja <wanjamaria78@gmail.com>
2023-02-19 09:54:01 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m <= 'z');
+{
+	if
+	((m != 'q' && m != 'e') && m <= 'z')
+		putchar (m);
+	m++;
+}
+return (0);
+}
