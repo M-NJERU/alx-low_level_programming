6bd9d11cf3776474e12539ba55a683382b5ef461
wanja <wanjamaria78@gmail.com>
2023-02-19 12:08:15 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0)success
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v < 10; v++)
+		printf("%d", v);
+	putchar('\n');
+	return (0);
+}
